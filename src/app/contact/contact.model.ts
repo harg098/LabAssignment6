14a3f05b8ff75bfe69d5060fd3b63ecb446dbe0e
Harinder Gill <harinder112@hotmail.com>
2018-11-28 14:39:01 -0800

    fixed errors-done

--- src/app/contact/contact.model.ts
+++ src/app/contact/contact.model.ts
@@ -1,6 +1,4 @@
-
 interface IContact {
-
     id?: number;
     firstName?: string;
     lastName?: string;
@@ -8,8 +6,15 @@ interface IContact {
     phone?: string;
     editing?: boolean;
 }
+
 export class Contact {
 
+    public id?: number;
+    public firstName?: string;
+    public lastName?: string;
+    public email?: string;
+    public phone?: string;
+    public editing?: boolean;
 
     constructor(contact: IContact) {
         contact.editing = this.setState(contact);
@@ -18,17 +23,16 @@ export class Contact {
 
     setState(contact: IContact) {
 
-        if (contact == null || Object.keys(contact).length == 0) {
+        if(contact == null || Object.keys(contact).length == 0) {
             return true;
         }
-
         let editing = false;
         Object.keys(contact).forEach((key) => {
-            if (contact[key] == null) {
+            console.log('from setState...', contact[key]);
+            if(contact[key] == null) {
                 editing = true;
             }
         });
-
         return editing;
     }
 }
